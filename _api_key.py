6a2c18c5dcf5874e022c2e024a78fa4ba40575c4
Zhijun-Xu <167455948+Zhijun-Xu@users.noreply.github.com>
2024-04-19 18:19:20 +0800

    Add files via upload

---
+++ _api_key.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+def get_openai_api_key():
+    # Use your own openai api key here
+    openai_api_key = "xxxx"
+    return openai_api_key
+
+def get_google_api_key():
+    # Use your own google api key here
+    google_api_key = "xxxx"
+    return google_api_key
+
+def get_claude_api_key():
+    # Use your own anthropic api key here
+    claude_api_key = "xxxx"
+    return claude_api_key
+
